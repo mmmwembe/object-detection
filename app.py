ca96eecd3fd1af19cf4738c094033722507d19d1
Michael M <mmm111@hotmail.com>
2022-09-01 17:46:44 -0400

    changes

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ bootstrap = Bootstrap(app)
 
 @app.route('/')
 def home():
-  return render_template('classify-images.html')
+  return render_template('detect-images.html')
 
 
 if __name__ == '__main__':
