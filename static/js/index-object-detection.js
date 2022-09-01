ef4db1bca5bcb3228d4b27401466212f3d67d58d
Michael M <mmm111@hotmail.com>
2022-09-01 18:05:07 -0400

    changes

--- static/js/index-object-detection.js
+++ static/js/index-object-detection.js
@@ -1,44 +1,9 @@
 // based on https://codelabs.developers.google.com/codelabs/tensorflowjs-object-detection
-const video = document.getElementById("webcam");
+const video = document.getElementById("my-image");
 const liveView = document.getElementById("liveView");
 const demosSection = document.getElementById("demos");
 const enableWebcamButton = document.getElementById("webcamButton");
 
-// Check if webcam access is supported.
-function getUserMediaSupported() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
-}
-
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will
-// define in the next step.
-if (getUserMediaSupported()) {
-  enableWebcamButton.addEventListener("click", enableCam);
-} else {
-  console.warn("getUserMedia() is not supported by your browser");
-}
-
-// Enable the live webcam view and start classification.
-function enableCam(event) {
-  // Only continue if the COCO-SSD has finished loading.
-  if (!model) {
-    return;
-  }
-
-  // Hide the button once clicked.
-  event.target.classList.add("removed");
-
-  // getUsermedia parameters to force video but not audio.
-  const constraints = {
-    video: true,
-  };
-
-  // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
-    video.srcObject = stream;
-    video.addEventListener("loadeddata", predictWebcam);
-  });
-}
 
 // Store the resulting model in the global scope of our app.
 var model = undefined;
@@ -102,5 +67,5 @@ function predictWebcam() {
     }
   }
   // Call this function again to keep predicting when the browser is ready.
-  window.requestAnimationFrame(predictWebcam);
+  // window.requestAnimationFrame(predictWebcam);
 }
